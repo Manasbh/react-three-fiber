9c22655996e7d3b2b7c2f8654ce79d30832fb90d
Manas <manas_bhasin@yahoo.com>
2023-07-20 18:30:49 +0530

    gtkrktest

--- react-three-fiber-boilerplate/src/app.jsx
+++ react-three-fiber-boilerplate/src/app.jsx
@@ -5,7 +5,7 @@ import { Box } from './Box'
 export function App(){
     return(
     <Canvas camera={{ position: [0, 0, 2] }}>
-       <Box position={[-0.75, 0, 0]} name="A" wireframe={false} />
+       <Box position={[-0.75, 0, 0.01]} name="A" wireframe={false} />
        <Box position={[0.75, 0, 0]} name="B"  wireframe={true} />
     </Canvas>
     )
